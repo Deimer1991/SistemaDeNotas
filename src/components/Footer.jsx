51dfde4c0f29ce9e9f9a29fbffb9eef47c95142c
Deimer1991 <deimer.yav@hotmail.com>
2026-03-02 20:09:31 -0500

    Se crea la rama dev

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,3 @@
 // Hacer el Footer 
 //Esto lo realiza un participante del grupo 
+//El participante tiene su rama para poderlo hacer 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,3 @@
 // Hacer el Navbar
 // Esto lo realiza un participante del grupo 
+// El participante debe de hacerlo para poder hacer los cambios en su rama
