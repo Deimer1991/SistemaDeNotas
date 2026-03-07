ae8121e0541c6346951162e578fc540d275cdf60
jfuz91 <felipeuz.07@gmail.com>
2026-03-07 08:17:24 -0500

    Feature: se añade navbar con su css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,18 @@
-// Hacer el Navbar
-// Esto lo realiza un participante del grupo 
+import './Nav.css';
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Inicio</li>
+        <li>Iniciar Sesión</li>
+        <li>Registrarse</li>
+        <li>Materias</li>
+        <li>Profesores</li>
+        <li>Reportes</li>
+        <li>Cerrar Sesión</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
